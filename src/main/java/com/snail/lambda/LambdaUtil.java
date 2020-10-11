30fa7b03a8b3a433a35592093cb0593a366c8e56
a1215305395 <38575998+a1215305395@users.noreply.github.com>
2020-10-11 13:25:09 +0800

    Lambda工具类增加去重方法

--- src/main/java/com/snail/lambda/LambdaUtil.java
+++ src/main/java/com/snail/lambda/LambdaUtil.java
@@ -1,8 +1,11 @@
 package com.snail.lambda;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -76,6 +79,82 @@ public enum LambdaUtil {
             .collect(Collectors.toMap(keyFun, Function.identity(), replaceMergeFunction()));
 
 
+    }
+
+    /**
+     * 列表去重
+     *
+     * @param sourceList 源列表
+     * @param keyFun     生成键的方法(使用这个键来去重)
+     * @param isParallel 是否使用parallelStream
+     * @param <T>        源类型
+     * @param <K>        键类型
+     * @return 去重后的list
+     */
+    public static <T, K> List<T> distinct(List<T> sourceList, Function<T, K> keyFun, boolean isParallel) {
+        return (isParallel ? sourceList.parallelStream() : sourceList.stream())
+            .filter(distinctPredicate(sourceList, keyFun, isParallel))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * 生成一个用于去重的Predicate(在filter中使用)
+     * Note 会保留重复的第一个(parallelStream不一定)
+     *
+     * @param sourceList 源列表(用于生成去重集合的大小)
+     * @param keyFun     生成键的方法(使用这个键来去重)
+     * @param isParallel 是否使用在parallelStream中
+     * @param <T>        源类型
+     * @param <K>        源类型(请确保key类型hashCode())
+     * @return Predicate
+     */
+    public static <T, K> Predicate<T> distinctPredicate(List<T> sourceList, Function<T, K> keyFun, boolean isParallel) {
+
+//        用于存储去重集
+        Set<Object> keySet;
+//        用于null值
+        Object oForNull = new Object();
+
+//        选择去重集的具体实现类
+        if (isParallel) {
+            keySet = new ConcurrentSkipListSet<>();
+        } else {
+            keySet = sourceList == null ? new TreeSet<>() : new HashSet<>(sourceList.size());
+        }
+
+        return (target) -> {
+            K key = keyFun.apply(target);
+            return keySet.add(key == null ? oForNull : key);
+        };
+
+    }
+
+    /**
+     * 生成一个用于去重的Predicate(在filter中使用)
+     * Note 会保留重复的第一个，不能用于ParallelStream
+     *
+     * @param keyFun 生成键的方法(使用这个键来去重)
+     * @param <T>    源类型
+     * @param <K>    源类型(请确保key类型hashCode())
+     * @return Predicate
+     */
+    public static <T, K> Predicate<T> distinctPredicate(Function<T, K> keyFun) {
+        return distinctPredicate(null, keyFun, false);
+    }
+
+    /**
+     * 列表去重
+     *
+     * @param sourceList 源列表
+     * @param keyFun     生成键的方法(使用这个键来去重)
+     * @param <T>        源类型
+     * @param <K>        键类型
+     * @return 去重后的list
+     */
+    public static <T, K> List<T> distinct(List<T> sourceList, Function<T, K> keyFun) {
+        return sourceList.stream()
+            .filter(distinctPredicate(sourceList, keyFun, false))
+            .collect(Collectors.toList());
     }
 
 
