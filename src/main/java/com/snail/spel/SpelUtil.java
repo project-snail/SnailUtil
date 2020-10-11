27b6af524b8cacc2025052a25ff2c58f6af0b443
a1215305395 <38575998+a1215305395@users.noreply.github.com>
2020-10-11 13:00:49 +0800

    增加AspectSpel工具类

---
+++ src/main/java/com/snail/spel/SpelUtil.java
@@ -0,0 +1,172 @@
+package com.snail.spel;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelEvaluationException;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public enum SpelUtil {
+    ;
+    private static ExpressionParser parser = new SpelExpressionParser();
+
+    private static LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
+
+    /**
+     * 解析el值 map上下文
+     *
+     * @param expression el表达式
+     * @param context    当前上下文
+     * @param valueClass 期待解析后的类型
+     * @param <T>        解析后的类型
+     * @return 值
+     */
+    public static <T> T getValue(String expression, EvaluationContext context, Class<T> valueClass) {
+        Expression exp = parser.parseExpression(expression);
+        try {
+            return exp.getValue(context, valueClass);
+        } catch (SpelEvaluationException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 解析el值 方法入参上下文
+     *
+     * @param expression el表达式
+     * @param method     目标方法
+     * @param args       目标方法的入参
+     * @param valueClass 期待解析后的类型
+     * @param <T>        解析后的类型
+     * @return 值
+     */
+    public static <T> T getValue(String expression, Method method, Object[] args, Class<T> valueClass) {
+        return getValue(expression, parseMethodToContext(method, args), valueClass);
+
+    }
+
+    /**
+     * 生成一个解析器 map上下文
+     *
+     * @param variableMap 当前上下文 <名称, 值>
+     * @return 解析器
+     */
+    public static SpelFunction generateSpelFunction(Map<String, Object> variableMap) {
+        EvaluationContext evaluationContext = parseVariableMapToContext(variableMap);
+        return new SpelFunction() {
+            @Override
+            public <T> T getValue(String expression, Class<T> valueClass) {
+                return SpelUtil.getValue(expression, evaluationContext, valueClass);
+            }
+        };
+    }
+
+    /**
+     * 生成一个解析器 方法入参上下文
+     *
+     * @param method 目标方法
+     * @param args   目标方法的入参
+     * @return 解析器
+     */
+    public static SpelFunction generateSpelFunction(Method method, Object[] args) {
+        return generateSpelFunction(parseMethodToVariableMap(method, args));
+    }
+
+    /**
+     * 将 方法入参上下文 转换成 map上下文
+     *
+     * @param method 目标方法
+     * @param args   目标方法的入参
+     * @return map上下文
+     */
+    private static Map<String, Object> parseMethodToVariableMap(Method method, Object[] args) {
+
+        if (method == null || args == null) {
+            return Collections.emptyMap();
+        }
+
+//        获取入参名称
+        String[] parameterNames = discoverer.getParameterNames(method);
+
+//        入参列表和入参值长度不匹配 返回空map
+        if (parameterNames == null || parameterNames.length != args.length) {
+            return Collections.emptyMap();
+        }
+
+        HashMap<String, Object> variableMap = new HashMap<>(parameterNames.length);
+        for (int i = 0; i < parameterNames.length; i++) {
+            variableMap.put(parameterNames[i], args[i]);
+        }
+        return variableMap;
+    }
+
+    /**
+     * 将map上下文转换成EvaluationContext
+     *
+     * @param variableMap map上下文
+     * @return
+     */
+    private static EvaluationContext parseVariableMapToContext(Map<String, Object> variableMap) {
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        if (variableMap != null) {
+            for (Map.Entry<String, Object> entry : variableMap.entrySet()) {
+                context.setVariable(entry.getKey(), entry.getValue());
+            }
+        }
+        return context;
+    }
+
+    /**
+     * 直接把方法和入参转换成EvaluationContext
+     *
+     * @param method 方法
+     * @param args   入参
+     * @return
+     */
+    private static EvaluationContext parseMethodToContext(Method method, Object[] args) {
+        return parseVariableMapToContext(parseMethodToVariableMap(method, args));
+    }
+
+    @FunctionalInterface
+    public interface SpelFunction {
+        /**
+         * 解析方法
+         *
+         * @param expression el表达式
+         * @param valueClass 期望表达式解析后的类型
+         * @param <T>        类型
+         * @return 值
+         */
+        <T> T getValue(String expression, Class<T> valueClass);
+
+        /**
+         * 从el表达式中解析值
+         *
+         * @param expression el表达式
+         * @param supplier   如果表达式解析后为null，则使用此Supplier提供的值
+         * @param valueClass 期望表达式解析后的类型
+         * @param <T>        类型
+         * @return 值
+         */
+        default <T> T tryParseValue(String expression, Supplier<T> supplier, Class<T> valueClass) {
+            T value;
+            if (StringUtils.isNotBlank(expression)) {
+                value = getValue(expression, valueClass);
+                if (value != null) {
+                    return value;
+                }
+            }
+            return supplier.get();
+        }
+    }
+
+}
---
+++ src/main/java/com/snail/spel/aspect/AspectSpelUtil.java
@@ -0,0 +1,48 @@
+package com.snail.spel.aspect;
+
+
+import com.snail.spel.SpelUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
+
+public enum AspectSpelUtil {
+    ;
+
+    /**
+     * 从切面生成spel解析器
+     *
+     * @param joinPoint 切面入参
+     * @return spel解析器
+     */
+    public static SpelUtil.SpelFunction generateSpelFunction(ProceedingJoinPoint joinPoint) {
+
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+
+        Object[] args = joinPoint.getArgs();
+
+        return SpelUtil.generateSpelFunction(method, args);
+
+    }
+
+    /**
+     * 从切面生成spel解析器 带condition(方便判断这次el是否需要解析)
+     *
+     * @param joinPoint           切面入参
+     * @param conditionExpression conditionEl表达式, 如果condition解析为null或false 返回值为null
+     * @return spel解析器
+     */
+    public static SpelUtil.SpelFunction generateSpelFunctionWithCondition(
+        ProceedingJoinPoint joinPoint, String conditionExpression
+    ) {
+
+        SpelUtil.SpelFunction spelFunction = generateSpelFunction(joinPoint);
+
+        Boolean condition = spelFunction.getValue(conditionExpression, Boolean.class);
+
+        return condition != null && condition ? spelFunction : null;
+
+    }
+
+}
